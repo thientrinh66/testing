22a834d2d7858f22bc5262e3812ea471a8c21c03
thientrinh66 <57164289+thientrinh66@users.noreply.github.com>
2022-04-11 22:06:46 +0700

    Add files via upload

---
+++ playcanvasdotcom/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1387234.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 77092081, 77092104, 77092067, 77092086, 77092069, 77092063, 77092064, 77092099, 77092093, 77092074, 77092076, 77092075, 77092107, 77092046, 77092103 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
